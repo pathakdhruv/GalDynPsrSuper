32d5bbd059d419171ed8e0d76d931cb4da55209f
pathakdhruv <pathakdhruv9786@gmail.com>
2022-06-16 14:52:37 +0530

    Some improvements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="GalDynPsrSuper",
-    version="v0.0.1",
+    version="v0.0.01",
     author="Dhruv Pathak",
     author_email="pathakdhruv9786@gmail.com",
     license='New BSD',
