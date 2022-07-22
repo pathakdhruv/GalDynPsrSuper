d7f0211cb3ef728aaba2e0c07fd890142497e38b
pathakdhruv <pathakdhruv9786@gmail.com>
2022-07-22 14:15:14 +0530

    Some improvements

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="GalDynPsrSuper",
-    version="v0.0.5",
+    version="v0.0.1",
     author="Dhruv Pathak",
     author_email="pathakdhruv9786@gmail.com",
     license='New BSD',
-    description="A package for estimating contribution of dynamical terms in the first and second derivatives of frequencies (both spin and orbital) of pulsars.",
+    description="A package for estimating contribution of dynamical terms in the first and second derivatives of frequencies as well as periods (both spin and orbital) of pulsars.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pathakdhruv/GalDynPsrSuper",
